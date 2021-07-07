8fc93123591e42ec0dd9e6c5718f6443b6dddb12
Sabina Abdulaliieva <77077884+Sabinka1207@users.noreply.github.com>
2021-07-07 23:15:27 +0300

    hbs template realised

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-import './sass/main.scss';
+import cardsTemplate from './tempates/cards.hbs'
+import dishes from './menu.json'
+
+// console.log(dishes.forEach(dish => cardsTemplate(dish).join(' ')))
+const menuList = document.querySelector(".js-menu")
+const dishesGrid = dishes.map(cardsTemplate).join(' ')
+
+menuList.insertAdjacentHTML('afterbegin', dishesGrid)
