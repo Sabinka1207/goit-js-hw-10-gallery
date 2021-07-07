5cdba2130420f6d7036aca88b5de2536fb439a65
Sabina Abdulaliieva <77077884+Sabinka1207@users.noreply.github.com>
2021-07-08 01:29:14 +0300

    hw10 done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,43 @@
 import cardsTemplate from './tempates/cards.hbs'
 import dishes from './menu.json'
 
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
 // console.log(dishes.forEach(dish => cardsTemplate(dish).join(' ')))
 const menuList = document.querySelector(".js-menu")
-const dishesGrid = dishes.map(cardsTemplate).join(' ')
+const body = document.querySelector("body")
+const switcher = document.querySelector("#theme-switch-toggle")
 
+const dishesGrid = dishes.map(cardsTemplate).join(' ')
 menuList.insertAdjacentHTML('afterbegin', dishesGrid)
+
+if (localStorage.length === 0) {
+    localStorage.setItem("theme", Theme.LIGHT)
+    body.classList.add(Theme.LIGHT)
+}
+
+if (localStorage.getItem("theme") === Theme.LIGHT) {
+      body.classList.add(Theme.LIGHT)  
+}
+
+if (localStorage.getItem("theme") === Theme.DARK) {
+    body.classList.add(Theme.DARK)
+    switcher.checked = true
+}
+
+switcher.addEventListener("change", onThemeChange)
+
+function onThemeChange() {
+    if (switcher.checked === true) {
+        body.classList.add(Theme.DARK)
+        body.classList.remove(Theme.LIGHT)
+        localStorage.setItem("theme", Theme.DARK)
+    } else {
+        body.classList.add(Theme.LIGHT)
+        body.classList.remove(Theme.DARK)
+        localStorage.setItem("theme", Theme.LIGHT)
+    }
+}
